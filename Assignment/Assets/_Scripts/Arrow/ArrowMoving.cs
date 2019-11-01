ddcf68ea1911cc44fe89d5959ad2b5176c7406eb
JerryHe <jerry_he2012@yahoo.com>
2019-11-01 10:09:29 -0400

    Added Bow Animimation

--- Assignment/Assets/_Scripts/Arrow/ArrowMoving.cs
+++ Assignment/Assets/_Scripts/Arrow/ArrowMoving.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ArrowMoving : MonoBehaviour
 {
     public float speed = 10;
+    public float forceLimit = 0.76f;
     public float minCheck = 0.25f;
     public bool tfFlying = true;
     public bool tfWillBounce = false;
@@ -15,16 +16,23 @@ public class ArrowMoving : MonoBehaviour
 
     private bool tfshot = false;    
     private GameObject preBouncePlate = null;
+    private GameObject theBow = null;
+    private GameObject theRope = null;
     private float preDistance = 0.0f;
     private MouseManager theMouse;
     private Quaternion originalRotation;
     private Vector3 originalPosition;
+    private Vector3 originalTailPosition;
     // Start is called before the first frame update
     void Start()
     {
         originalRotation = gameObject.transform.rotation;
         originalPosition = gameObject.transform.position;
         theMouse = GameObject.Find("MouseManager").GetComponent<MouseManager>();
+        theBow = GameObject.Find("Bow");
+        theRope = GameObject.Find("Rope");
+        theRope.GetComponent<RopeReference>().theArrowTail = gameObject.transform.Find("ArrowTail").gameObject;
+        originalTailPosition = gameObject.transform.Find("ArrowTail").localPosition;
     }
 
     // Update is called once per frame
@@ -37,13 +45,38 @@ public class ArrowMoving : MonoBehaviour
 
         if (tfMouseSetting)
         {
+            gameObject.transform.localPosition = Vector3.zero;
             Vector3 mousePosition = theMouse.GetMousePosition();
+            Vector3 tailPositionDifference = mousePosition - gameObject.transform.Find("ArrowTail").position;
+            float forceDistance = Vector3.Distance(mousePosition, gameObject.transform.position);
+            float tailDistance = Vector3.Distance(mousePosition, gameObject.transform.Find("ArrowTail").position);
             gameObject.transform.LookAt(mousePosition);
             gameObject.transform.eulerAngles = new Vector3(0, gameObject.transform.eulerAngles.y + 180.0f, 0);
+            if (forceDistance < forceLimit)
+            {
+                gameObject.transform.localPosition += tailPositionDifference;
+            }
+            else
+            {
+                gameObject.transform.localPosition += ((tailPositionDifference * (forceLimit / tailDistance)) + (gameObject.transform.position - gameObject.transform.Find("ArrowTail").position));
+            }
+            theBow.transform.LookAt(mousePosition);
+            theBow.transform.eulerAngles = new Vector3(0, theBow.transform.eulerAngles.y, 90.0f);
+            theRope.GetComponent<RopeReference>().UpdateRopePosition();
 
-            speed = Vector3.Distance(mousePosition, gameObject.transform.position) * speedScale;
+            if (forceDistance < forceLimit)
+            {
+                speed = forceDistance * speedScale;
+            }
+            else
+            {
+                speed = forceLimit * speedScale;
+            }
             if (Input.GetMouseButtonDown(1))
             {
+                gameObject.transform.localPosition = Vector3.zero;
+                gameObject.transform.Find("ArrowTail").localPosition = Vector3.zero;
+                theRope.GetComponent<RopeReference>().UpdateRopePosition();
                 tfshot = true;
                 tfFlying = true;
                 tfMouseSetting = false;
@@ -56,6 +89,7 @@ public class ArrowMoving : MonoBehaviour
             {
                 if (Input.GetKeyDown(KeyCode.R))
                 {
+                    gameObject.transform.Find("ArrowTail").localPosition = originalTailPosition;
                     gameObject.transform.rotation = originalRotation;
                     gameObject.transform.position = originalPosition;
                     preBouncePlate = null;
---
+++ Assignment/Assets/_Scripts/Arrow/RopeReference.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeReference : MonoBehaviour
+{
+    public GameObject theArrowTail = null;
+
+    [SerializeField]
+    public GameObject theRopeHeadOne = null;
+    [SerializeField]
+    public GameObject theRopeHeadTwo = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateRopePosition()
+    {
+        gameObject.GetComponent<LineRenderer>().SetPosition(0, theRopeHeadOne.transform.position);
+        gameObject.GetComponent<LineRenderer>().SetPosition(1, theArrowTail.transform.position);
+        gameObject.GetComponent<LineRenderer>().SetPosition(2, theRopeHeadTwo.transform.position);
+    }
+}
