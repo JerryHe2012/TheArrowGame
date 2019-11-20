3c2ea27bef34a0c2d0f7f46b904b70a183c72e5e
TrollingRookey <jerry_he2012@yahoo.com>
2019-11-20 13:29:38 +0800

    UpdatedMainUI

---
+++ Assignment/Assets/_Scripts/UI/MainMenuTextColorChange.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class MainMenuTextColorChange : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    [SerializeField]
+    private TextMeshProUGUI theText = null;
+    [SerializeField]
+    private Color enterColor = Color.blue;
+    [SerializeField]
+    private Color exitColor = Color.white;
+    [SerializeField]
+    private Color clickColor = Color.red;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        theText.color = enterColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        theText.color = exitColor;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        theText.color = clickColor;
+    }
+}
---
+++ Assignment/Assets/_Scripts/UI/RandomLoadingArrow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomLoadingArrow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!GetComponent<Animation>().isPlaying)
+        {
+            transform.localPosition = new Vector3(Random.Range(-5.0f, 5.0f), -10.0f, Random.Range(-5.0f, 5.0f));
+            transform.localEulerAngles = new Vector3(0, Random.Range(0.0f, 360.0f), 0);
+            GetComponent<Animation>().Play();
+        }
+    }
+}
