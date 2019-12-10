731a031d626f5b291fb006a5df59158c20d11c5d
JerryHe <jerry_he2012@yahoo.com>
2019-12-10 15:19:20 -0500

    Updated Aim Animation

--- Assignment/Assets/_Scripts/Arrow/PointerLaser.cs
+++ Assignment/Assets/_Scripts/Arrow/PointerLaser.cs
@@ -20,18 +20,27 @@ public class PointerLaser : MonoBehaviour
     {
         if (!gameObject.transform.parent.gameObject.GetComponent<ArrowMoving>().tfMouseSetting)
         {
+            lr.enabled = false;
             glowingPoint.SetActive(false);
-            gameObject.SetActive(false);
+        }
+        else
+        {
+            lr.enabled = true;
+            glowingPoint.SetActive(true);
         }
 
         lr.SetPosition(0, transform.position);
         RaycastHit hit;
         if (Physics.Raycast(transform.position, transform.forward, out hit))
         {
-            if (hit.collider)
+            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Wall"))
             {
                 lr.SetPosition(1, hit.point - (transform.forward * 0.2f));
                 glowingPoint.transform.position = hit.point;
+            }
+            else
+            {
+                lr.SetPosition(1, glowingPoint.transform.position - (transform.forward * 0.2f));
             }
         }
         else
